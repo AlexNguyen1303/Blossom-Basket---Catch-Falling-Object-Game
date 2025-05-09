fa98f357fab5f69859c6ac465f2e0759448cddac
Uyen Nguyen <alexnguyen1303@gmail.com>
2025-05-08 23:25:28 -0500

    set up basic  variable screen/color/basket/fallingitems and some function to spawn item

--- src/project.py
+++ src/project.py
@@ -1,11 +1,38 @@
 import pygame
 import random 
 
-pygame.init()
-WIDTH, HEIGHT = 600,800
-screen = pygame.display.set_mode((WIDTH, HEIGHT))
-pygame.display.set_caption("Blossom Basket")
+def main():
 
-clock = pygame.time.Clock()
-FPS = 60 
+    pygame.init()
 
+    #Screen
+    WIDTH, HEIGHT = 800,1000
+    screen = pygame.display.set_mode((WIDTH, HEIGHT))
+    pygame.display.set_caption("Blossom Basket")
+    clock = pygame.time.Clock()
+    FPS = 60 
+    #Colors
+    WHITE = (255,255,255)
+    PINK = (255,182,193)
+    #Basket
+    basket_img = pygame.Surface((100,50))
+    basket_img.fill(PINK)
+    basket_rect = basket_img.get_rect(center=(WIDTH//2, HEIGHT - 50))
+    basket_speed = 7
+    # Falling items 
+    item_img = pygame.Surface((30,30))
+    item_img.fill((200,100,150))
+    items = []
+    #Score 
+    score = 0 
+    font = pygame.font.SysFont(None,36)
+
+    #Function to spawn items
+    def spawn_item():
+        x = random.randint(0,WIDTH - 30)
+        y = -30
+        items.append(pygame.Rect(x,y,30, 30))
+
+   
+if __name__ == "__main__":
+    main()
