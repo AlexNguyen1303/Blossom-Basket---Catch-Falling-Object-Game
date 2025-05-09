c0eb05d22b5ee35c4705ac6a34cdff5ef967905e
Uyen Nguyen <alexnguyen1303@gmail.com>
2025-05-08 22:50:10 -0500

    Setup Game window and frame rate

--- src/project.py
+++ src/project.py
@@ -1,1 +1,11 @@
-print("Hello World !!!")
+import pygame
+import random 
+
+pygame.init()
+WIDTH, HEIGHT = 600,800
+screen = pygame.display.set_mode((WIDTH, HEIGHT))
+pygame.display.set_caption("Blossom Basket")
+
+clock = pygame.time.Clock()
+FPS = 60 
+
